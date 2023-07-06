f8a22812fb26f6df189f380bd2f1f7750567623a
Peter Muessig <peter.muessig@sap.com>
2023-07-06 13:52:33 +0200

    first commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+  "./BaseController",
+  "sap/m/MessageBox"
+], function (BaseController, MessageBox) {
+  "use strict";
+
+  return BaseController.extend("$namespace$.controller.Main", {
+    sayHello: function() {
+      MessageBox.show("Hello World!");
+    }
+  });
+
+});
